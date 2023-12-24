d0675bda9ca4db4f52f68ddf076019b6d089654b
Laxman-creator <127328113+Laxman-creator@users.noreply.github.com>
2023-12-24 19:40:05 +0530

    changes done

--- src/test/java/cucumber/options/TestRunner.java
+++ src/test/java/cucumber/options/TestRunner.java
@@ -2,7 +2,7 @@ package cucumber.options;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(features ="D:\\java eclipse\\com.sharkNinja\\src\\test\\java\\features",glue = {"stepDefination","helper"},tags = "@test",
+@CucumberOptions(features ="D:\\java eclipse\\com.sharkNinja\\src\\test\\java\\features",glue = {"stepDefination","helper"},tags = "@reg",
 plugin = {"pretty","json:target/jsonReports/report.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
