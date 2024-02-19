63a8189f7e973b647964c7e02301f1193dee04e4
Laxman-creator <127328113+Laxman-creator@users.noreply.github.com>
2024-02-19 16:48:27 +0530

    feature file path modified in test runner

--- src/test/java/helper/BaseClass.java
+++ src/test/java/helper/BaseClass.java
@@ -1,7 +1,5 @@
 package helper;
 
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -35,130 +33,130 @@ import io.cucumber.java.Scenario;
 
 public class BaseClass {
 
-    public static WebDriver driver;
-    public static WebDriverWait wait;
-    public static Properties prop;
+	public static WebDriver driver;
+	public static WebDriverWait wait;
+	public static Properties prop;
 
-    static {
-        try {
-            FileInputStream file = new FileInputStream(
-                    System.getProperty("user.dir") + "/src/test/java/resources/env.properties");
-            prop = new Properties();
-            prop.load(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	static {
+		try {
+			FileInputStream file = new FileInputStream(
+					System.getProperty("user.dir") + "/src/test/java/resources/env.properties");
+			prop = new Properties();
+			prop.load(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Before
-    public void setup() {
-        String br = prop.getProperty("browser");
-        if (br.equals("chrome")) {
-            ChromeOptions option = new ChromeOptions();
-            option.addArguments("--incognito");
-            driver = new ChromeDriver(option);
-        } else if (br.equals("firefox")) {
-            FirefoxOptions option = new FirefoxOptions();
-            option.addArguments("--incognito");
-            driver = new FirefoxDriver(option);
-        } else if (br.equals("edge")) {
-            EdgeOptions option = new EdgeOptions();
-            option.addArguments("--incognito");
-            driver = new EdgeDriver(option);
-        }
+	@Before
+	public void setup() {
+		String br = prop.getProperty("browser");
+		if (br.equals("chrome")) {
+			ChromeOptions option = new ChromeOptions();
+			option.addArguments("--incognito");
+			driver = new ChromeDriver(option);
+		} else if (br.equals("firefox")) {
+			FirefoxOptions option = new FirefoxOptions();
+			option.addArguments("--incognito");
+			driver = new FirefoxDriver(option);
+		} else if (br.equals("edge")) {
+			EdgeOptions option = new EdgeOptions();
+			option.addArguments("--incognito");
+			driver = new EdgeDriver(option);
+		}
 
-        driver.get(prop.getProperty("url"));
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
+		driver.get(prop.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
 
-    public void explicitwait(WebElement elm, String type) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+	public void explicitwait(WebElement elm, String type) {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        switch (type) {
-            case "visibilityof":
-                wait.until(ExpectedConditions.visibilityOf(elm));
-                break;
-            case "alertisPresent":
-                wait.until(ExpectedConditions.alertIsPresent());
-                break;
-            case "invisibilityofelement":
-                wait.until(ExpectedConditions.invisibilityOf(elm));
-                break;
-        }
-    }
+		switch (type) {
+		case "visibilityof":
+			wait.until(ExpectedConditions.visibilityOf(elm));
+			break;
+		case "alertisPresent":
+			wait.until(ExpectedConditions.alertIsPresent());
+			break;
+		case "invisibilityofelement":
+			wait.until(ExpectedConditions.invisibilityOf(elm));
+			break;
+		}
+	}
 
-    public void handleWindow(Set<String> set, String title) {
-        List<String> list = new ArrayList<>(set);
-        for (int i = 0; i < list.size(); i++) {
-            String actTitle = driver.switchTo().window(list.get(i)).getTitle();
-            if (actTitle.equals(title)) {
-                driver.switchTo().window(list.get(i));
-            }
-        }
-    }
+	public void handleWindow(Set<String> set, String title) {
+		List<String> list = new ArrayList<>(set);
+		for (int i = 0; i < list.size(); i++) {
+			String actTitle = driver.switchTo().window(list.get(i)).getTitle();
+			if (actTitle.equals(title)) {
+				driver.switchTo().window(list.get(i));
+			}
+		}
+	}
 
-    public void windowalert(String type) {
-        Alert a = driver.switchTo().alert();
-        switch (type) {
-            case "accept":
-                a.accept();
-                break;
-            case "dismiss":
-                a.dismiss();
-                break;
-        }
-    }
+	public void windowalert(String type) {
+		Alert a = driver.switchTo().alert();
+		switch (type) {
+		case "accept":
+			a.accept();
+			break;
+		case "dismiss":
+			a.dismiss();
+			break;
+		}
+	}
 
-    public void clickonelement(WebElement ele) {
-        try {
-            ele.click();
-        } catch (Exception e) {
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript("arguments[0].click();", ele);
-        }
-    }
+	public void clickonelement(WebElement ele) {
+		try {
+			ele.click();
+		} catch (Exception e) {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].click();", ele);
+		}
+	}
 
-    public void validateText(WebElement ele, String expectedText) {
-        String actualText = ele.getText();
-        Assert.assertEquals(actualText, expectedText);
-    }
+	public void validateText(WebElement ele, String expectedText) {
+		String actualText = ele.getText();
+		Assert.assertEquals(actualText, expectedText);
+	}
 
-    public void validateSizeOfList(List<WebElement> list, int size) {
-        int sizeOflist = list.size();
-        Assert.assertEquals(sizeOflist, size);
-    }
+	public void validateSizeOfList(List<WebElement> list, int size) {
+		int sizeOflist = list.size();
+		Assert.assertEquals(sizeOflist, size);
+	}
 
-    @After
-    public void tearDown(Scenario s) {
-        try {
-            if (s.isFailed()) {
-                TakesScreenshot ts = (TakesScreenshot) driver;
-                File src = ts.getScreenshotAs(OutputType.FILE);
-                File target = new File(System.getProperty("user.dir") + "/Screenshots/" + s.getName() + ".png");
-                FileUtils.copyFile(src, target);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
-        }
-    }
+	@After
+	public void tearDown(Scenario s) {
+		try {
+			if (s.isFailed()) {
+				TakesScreenshot ts = (TakesScreenshot) driver;
+				File src = ts.getScreenshotAs(OutputType.FILE);
+				File target = new File(System.getProperty("user.dir") + "/Screenshots/" + s.getName() + ".png");
+				FileUtils.copyFile(src, target);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			driver.quit();
+		}
+	}
 
-    public void handleFrame(String type, String value) {
-        switch (type) {
-            case "name":
-                driver.switchTo().frame(value);
-                break;
-            case "index":
-                driver.switchTo().frame(Integer.parseInt(value));
-        }
-    }
+	public void handleFrame(String type, String value) {
+		switch (type) {
+		case "name":
+			driver.switchTo().frame(value);
+			break;
+		case "index":
+			driver.switchTo().frame(Integer.parseInt(value));
+		}
+	}
 
-    public void explicitwaitForlist(List<WebElement> list) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfAllElements(list));
-    }
+	public void explicitwaitForlist(List<WebElement> list) {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfAllElements(list));
+	}
 }
--- src/test/java/page/CheckOutPage.java
+++ src/test/java/page/CheckOutPage.java
@@ -9,48 +9,47 @@ import helper.BaseClass;
 
 public class CheckOutPage extends BaseClass {
 
-	
-public void enterfirstname_last_name_and_zipcode(String string, String string2, String string3) {
-	    
-	driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys(string);
-	driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys(string2);
-	driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys(string3);
+	public void enterfirstname_last_name_and_zipcode(String string, String string2, String string3) {
+
+		driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys(string);
+		driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys(string2);
+		driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys(string3);
 	}
 
-	
 	public void clickoncontinuebutton() {
-	  WebElement elm= driver.findElement(By.xpath("//input[@value='CONTINUE']"));
-	  clickonelement(elm);
-	}
-	public void clickOnfinishbtn() {
-		 WebElement elm= driver.findElement(By.xpath("//a[normalize-space()='FINISH']"));
-		 clickonelement(elm);
+		WebElement elm = driver.findElement(By.xpath("//input[@value='CONTINUE']"));
+		clickonelement(elm);
 	}
 
-	
-	public void validatetext() {
-		WebElement elm=driver.findElement(By.xpath("//h2[normalize-space()='THANK YOU FOR YOUR ORDER']"));
-	    validateText(elm, "THANK YOU FOR YOUR ORDER");
+	public void clickOnfinishbtn() {
+		WebElement elm = driver.findElement(By.xpath("//a[normalize-space()='FINISH']"));
+		clickonelement(elm);
 	}
-public void validate_error_message_without_giving_zipcode() {
-		WebElement elm=driver.findElement(By.xpath("//h3[normalize-space()='Error: Postal Code is required']"));
+
+	public void validatetext() {
+		WebElement elm = driver.findElement(By.xpath("//h2[normalize-space()='THANK YOU FOR YOUR ORDER']"));
+		validateText(elm, "THANK YOU FOR YOUR ORDER");
+	}
+
+	public void validate_error_message_without_giving_zipcode() {
+		WebElement elm = driver.findElement(By.xpath("//h3[normalize-space()='Error: Postal Code is required']"));
 		assertEquals(elm.isDisplayed(), true);
 	}
 
-public void validate_error_message_without_giving_lastname() {
-	WebElement elm=driver.findElement(By.xpath("//h3[normalize-space()='Error: Last Name is required']"));
-	explicitwait(elm, "visibilityof");
-	assertEquals(elm.isDisplayed(), true);
-}
+	public void validate_error_message_without_giving_lastname() {
+		WebElement elm = driver.findElement(By.xpath("//h3[normalize-space()='Error: Last Name is required']"));
+		explicitwait(elm, "visibilityof");
+		assertEquals(elm.isDisplayed(), true);
+	}
 
-public void validate_error_message_without_giving_firstname() {
-	WebElement elm=driver.findElement(By.xpath("//h3[normalize-space()='Error: First Name is required']"));
-	assertEquals(elm.isDisplayed(), true);
-}
+	public void validate_error_message_without_giving_firstname() {
+		WebElement elm = driver.findElement(By.xpath("//h3[normalize-space()='Error: First Name is required']"));
+		assertEquals(elm.isDisplayed(), true);
+	}
 
-public void validate_error_message_without_giving_anyInput() {
-	WebElement elm=driver.findElement(By.xpath("//h3[normalize-space()='Error: First Name is required']"));
-	assertEquals(elm.isDisplayed(), true);
-}
+	public void validate_error_message_without_giving_anyInput() {
+		WebElement elm = driver.findElement(By.xpath("//h3[normalize-space()='Error: First Name is required']"));
+		assertEquals(elm.isDisplayed(), true);
+	}
 
 }
